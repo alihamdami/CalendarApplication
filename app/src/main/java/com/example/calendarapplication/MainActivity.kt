bcc846a589db38fbf85dfa4685a4528415371f91
alihamdami <77199231+alihamdami@users.noreply.github.com>
2023-07-18 00:25:00 +0300

    tab bar

--- app/src/main/java/com/example/calendarapplication/MainActivity.kt
+++ app/src/main/java/com/example/calendarapplication/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -36,13 +39,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(modifier = Modifier
-        .size(200.dp)
+    Column(modifier = Modifier
+        .padding(25.dp)
+        .fillMaxWidth()
     ){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier,
-            color = Color.BLUE
+        Text(text = "Hello, ")
+        Text(text = name)
     }
 }
 
